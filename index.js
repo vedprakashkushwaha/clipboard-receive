c7175169c4dca5600bf562364881d1264b1137a5
vedprakashkushwaha <vedprakashkushwaha94@gmail.com>
2022-11-12 23:26:03 +0530

    save data into redis

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+const requestIP = require('request-ip');
+const port = 3333;
+let redisClient;
+(async () => {
+    redisClient = redis.createClient();
+    redisClient.on("error", (error) => console.error(`Error : ${error}`));
+    await redisClient.connect();
+})();
+
+redisClient.on('connect', function () {
+    console.log('Connected!'); // Connected!
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/saveclipboard', async (req, res) => {
+    const ip = requestIP.getClientIp(req);
+    const data = { ip, data: req.body.text };
+    try {
+        const cacheResults = await redisClient.get('clipboardData');
+        if (cacheResults) {
+            let cacheData = JSON.parse(cacheResults);
+            cacheData.push(data);
+            await redisClient.set('clipboardData', JSON.stringify(cacheData));
+        } else {
+            await redisClient.set('clipboardData', JSON.stringify([data]));
+        }
+        res.json({ msg: 'data successflly saved' });
+    } catch (err) {
+        console.log("error: ", err)
+    }
+});
+
+app.get('/getclipboard', async (req, res) => {
+    try {
+        const cacheResults = await redisClient.get('clipboardData');
+        res.json(JSON.parse(cacheResults))
+    } catch (err) {
+        console.log("Error while fetching data: ", err)
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
